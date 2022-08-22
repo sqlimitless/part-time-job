20ba120e0d917382853ae9eba215b73a6e6699cc
lhjhoon <lhjhoon@gmail.com>
2022-08-22 23:32:40 +0900

    init - vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          달력을 만들어보자!
+        </h1>
+
+
+        <p class="subheading font-weight-regular">
+          1. 달력 <br>
+          1-1. 지점이름<br>
+          1-2. 직원들 일한 시간<br>
+          1-3. 시급<br>
+          1.4. 총 하루 일당<br>
+          <br>
+          2. 지점등록<br>
+          3. 직원 등록<br>
+          4. 지점에 직원등록<br>
+          5. 시급등록
+        </p>
+      </v-col>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-5">
+          Dev Page
+        </h2>
+        <v-row justify="center">
+          <li v-for='(list,i) in DevPage' :key="i">
+            <router-link :to='list.href' >{{ list.text }}</router-link>
+          </li>
+
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
 
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-});
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data () {
+    return {
+      DevPage: [
+        {
+          text: 'Main',
+          href: '/main',
+        }
+      ]
+    }
+  },
+})
+</script>
---
+++ src/views/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-btn color="success">Success</v-btn>
+  <v-btn color="error">Error</v-btn>
+  <v-btn color="warning">Warning</v-btn>
+  <v-btn color="info">Info</v-btn>
+</template>
+
+<script lang="ts">
+export default {
+  name: "MainView"
+}
+</script>
+
+<style scoped>
+
+</style>
