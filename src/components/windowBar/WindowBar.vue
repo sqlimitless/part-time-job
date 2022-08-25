c51d0af4cbd7db11db75fabde776468b59435be3
lhjhoon <lhjhoon@gmail.com>
2022-08-25 22:51:22 +0900

    init - 타이틀바 셋팅중

--- src/components/windowBar/WindowBar.vue
+++ src/components/windowBar/WindowBar.vue
@@ -3,20 +3,35 @@
     <v-system-bar
         window
         dark
+        style="-webkit-app-region: drag;"
     >
       <v-icon>mdi-message</v-icon>
       <span>10 unread messages</span>
       <v-spacer></v-spacer>
-      <v-icon>mdi-minus</v-icon>
+      <v-icon @click="minimize">mdi-minus</v-icon>
       <v-icon>mdi-checkbox-blank-outline</v-icon>
-      <v-icon>mdi-close</v-icon>
+      <v-icon @click="close">mdi-close</v-icon>
     </v-system-bar>
   </div>
 </template>
 
 <script>
+// import windowBar from "@/components/windowBar/windowBar.ts";
+// const { ipcRenderer } = require("electron");
+// import { ipcRenderer } from 'electron'
+
 export default {
-  name: "WindowBar"
+  name: "WindowBar",
+  methods: {
+    minimize() {
+      // windowBar.minimize()
+      // ipcRenderer.send('min', 'send');
+    },
+    close() {
+      // windowBar.close()
+      // ipcRenderer.send('min', 'send');
+    }
+  }
 }
 </script>
 
---
+++ src/components/windowBar/windowBar.ts
@@ -0,0 +1,17 @@
+// import {ipcRenderer} from "electron"
+//
+// const ipc = ipcRenderer
+
+// const elec = window.require('electron')
+// const remote = elec.remote
+
+export default {
+    minimize() {
+        // ipc.send('min')
+        // remote.getCurrentWindow().minimize()
+    },
+    close() {
+        // ipc.send('clo')
+        // remote.getCurrentWindow().close()
+    }
+}
